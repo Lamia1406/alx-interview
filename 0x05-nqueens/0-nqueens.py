955f3dc239ebf379fd5878b94c92ec51840228c4
Lamia Hamdi <101535283+Lamia1406@users.noreply.github.com>
2024-11-09 14:45:24 +0100

    solve nqueen problem

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,6 +1,27 @@
 #!/usr/bin/python3
 import sys
-
+solution = []
+def backtrack(queens, row=0):
+    
+    if row == int(queens):
+        print(solution)
+        return  
+    for j in range(int(queens)):
+        candidate = [row, j]
+        aligned = False
+        
+        for [m, n] in solution:
+            if n == j:
+                aligned = True
+                break
+            if abs(row - m) == abs(j - n):
+                aligned = True
+                break
+        
+        if not aligned:
+            solution.append(candidate)
+            backtrack(queens, row + 1)  
+            solution.pop() 
 if len(sys.argv) < 2:
     print("Usage: nqueens N")
     sys.exit(1)
@@ -14,3 +35,6 @@ except ValueError:
 if N < 4:
     print("N must be at least 4")
     sys.exit(1)
+
+
+backtrack(sys.argv[1])
